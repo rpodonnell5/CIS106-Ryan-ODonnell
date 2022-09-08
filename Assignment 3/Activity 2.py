aac831c9f5852449f6d0831d5cb66358e8074267
rpodonnell5 <111814196+rpodonnell5@users.noreply.github.com>
2022-09-08 19:31:41 -0400

    Add files via upload
    I made changes to give a more clear comment and also to individually state what each unit of age was

--- Assignment 3/Activity 2.py
+++ Assignment 3/Activity 2.py
@@ -1,9 +1,14 @@
-# This program converts an inputted age into approximate total lived months, days, hours, minutes, and secons
-print("Enter age")
+# This program converts an inputted age into approximate total lived months, days, hours, minutes, and seconds
+print("Please enter how old you are in years")
 age = float(input())
 months = age * 12
 days = age * 365
 hours = age * 365 * 24
 minutes = age * 365 * 24 * 60
 seconds = age * 365 * 24 * 60 * 60
-print(str(age) + "age is" + str(months) + "months" + str(days) + "days" + str(hours) + "hours" + str(minutes) + "minutes" + str(seconds) + "seconds")
+print("Your age is approximately equal to ...")
+print("Months = " + str(months) + " months")
+print("Days = " + str(days) + " days")
+print("Hours = " + str(hours) + " hours")
+print("Minutes = " + str(minutes) + " minutes")
+print("Seconds = " + str(seconds) + " seconds")
