7b442717a29dfe979065d1fec737308541d3c3b2
Dave Braunschweig <dave@itrain.ms>
2022-09-12 16:10:49 -0500

    Assignment 4 hints

--- Assignment 4/Activity 3.py
+++ Assignment 4/Activity 3.py
@@ -1,5 +1,8 @@
 # This program converts an input distance in miles to yards, feet, and inches.
 
+# Reference: 
+#   https://www.testingdocs.com/flowchart-to-convert-distance ...
+
 print("Please enter a distance in miles")
 distance = float (input())
 yards = distance * 1760
@@ -10,4 +13,3 @@ print ("Yards = " + str(yards) + " yards")
 print ("Feet = " + str(feet) + " feet") 
 print ("Inches = " + str(inches) + " inches")
 
-# Reference: https://www.testingdocs.com/flowchart-to-convert-distance-in-kilometers-to-miles/
