d63f77c1331b8e481a242acc6d0a7153e38aa6c4
rpodonnell5 <111814196+rpodonnell5@users.noreply.github.com>
2022-09-13 21:12:41 -0400

    Update Activity 3.py
    Deleted an extra space between reference and the output print string

--- Assignment 4/Activity 3.py
+++ Assignment 4/Activity 3.py
@@ -9,5 +9,4 @@ inches = distance * 63360
 
 print(str(distance) + "miles is equal to " + str(yards) + "yards, or " + str(feet) + "feet, or " + str(inches) + "inches.")
 
-
 # Reference: https://www.testingdocs.com/flowchart-to-convert-distance-in-kilometers-to-miles/
