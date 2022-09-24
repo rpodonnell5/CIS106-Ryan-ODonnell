61320de8ad10d7a99418bec46bc2d476f7a76bc7
rpodonnell5 <111814196+rpodonnell5@users.noreply.github.com>
2022-09-24 13:04:46 -0400

    Update Activity 2.py
    Made some formatting/line corrections

--- Assignment 6/Activity 2.py
+++ Assignment 6/Activity 2.py
@@ -1,26 +1,31 @@
-#Main
-#This program asks the user for an age in years converts the given age in years to months, days, hours, and seconds and displays the results.
-#References: #https://harpercollege.pressbooks.pub/programmingfundamentals/chapter/python-examples-3/
+# Main
+# This program converts an input age in years to the same age in months, days, hours, and seconds.
+# References: #https://harpercollege.pressbooks.pub/programmingfundamentals
 
 def get_years():
     print("Enter Age in years:")
     years = float(input())
+    
     return years
 
 def calculate_months(years):
     months = years*12
+    
     return months
 
 def calculate_days(years):
     days = years*365
+    
     return days
 
 def calculate_hours(years):
     hours = years*365*24
+    
     return hours
 
 def calculate_seconds(years):
     seconds = years*365*24*60*60
+    
     return seconds
 
 def display_result(years, months, days, hours, seconds):
