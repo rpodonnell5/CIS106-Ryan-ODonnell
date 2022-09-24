88f1e832140afe783e85246aa82adc65c4221701
rpodonnell5 <111814196+rpodonnell5@users.noreply.github.com>
2022-09-24 13:10:12 -0400

    Update Activity 2.py

--- Assignment 6/Activity 2.py
+++ Assignment 6/Activity 2.py
@@ -9,29 +9,39 @@ def get_years():
     
     return years
 
+
 def calculate_months(years):
     months = years * 12
     
     return months
+
 
 def calculate_days(years):
     days = years * 365
     
     return days
 
+
 def calculate_hours(years):
     hours = years * 365 * 24
     
     return hours
+
 
 def calculate_seconds(years):
     seconds = years * 365 * 24 * 60 * 60
     
     return seconds
 
-def display_result(years, months, days, hours, seconds):
-    print(str(years) + " Years is approximately equal to " + str(months) + " Months, " + str(days) + " Days, " + str(hours) + " Hours, and " + str(seconds) + " seconds.")
 
+def display_result(years, months, days, hours, seconds):
+    print(str(years) + " Years is approximately equal to " + 
+          str(months) + " Months, " + 
+          str(days) + " Days, " + 
+          str(hours) + " Hours, and " + 
+          str(seconds) + " seconds.")
+
+    
 def main():
     years = get_years()
     months = calculate_months(years)
@@ -39,5 +49,6 @@ def main():
     hours = calculate_hours(years)
     seconds = calculate_seconds(years)
     display_result(years, months, days, hours, seconds)
-    
+  
+
 main()
